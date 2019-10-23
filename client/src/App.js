8c5e8bdde506a803dfe934ac49d49e7610419e46
Kristen Johanson <43449427+klbjklbj@users.noreply.github.com>
2019-10-22 22:30:21 -0700

    through 10-5

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My React App</h1>
-    </div>
+    //wrap everything with routing capabilities
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path='/register' component={Register}/>
+          <Route exact path='/login' component={Login}/>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
